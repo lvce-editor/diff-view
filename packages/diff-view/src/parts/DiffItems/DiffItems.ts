c1b6c27b0de18f933608e2b7b09507b4b6a4bb49
Le Vivilet <levivilet@pm.me>
2026-04-16 17:58:45 +0200

    fix

--- packages/diff-view/src/parts/DiffItems/DiffItems.ts
+++ packages/diff-view/src/parts/DiffItems/DiffItems.ts
@@ -1,12 +1,5 @@
 import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
 
 export const isEqual = (oldState: DiffViewState, newState: DiffViewState): boolean => {
-  return (
-    oldState.allGroups === newState.allGroups &&
-    oldState.deltaY === newState.deltaY &&
-    oldState.items === newState.items &&
-    oldState.maxLineY === newState.maxLineY &&
-    oldState.minLineY === newState.minLineY &&
-    oldState.visibleItems === newState.visibleItems
-  )
+  return oldState.deltaY === newState.deltaY && oldState.maxLineY === newState.maxLineY && oldState.minLineY === newState.minLineY
 }

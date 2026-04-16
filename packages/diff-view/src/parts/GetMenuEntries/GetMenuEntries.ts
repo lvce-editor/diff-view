1c8f0341e3951d04fcccd4168affbe90be831033
Le Vivilet <levivilet@pm.me>
2026-04-16 17:59:18 +0200

    fix

--- packages/diff-view/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/diff-view/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,56 +1,5 @@
-import { MenuItemFlags } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
-import * as ViewletSourceControlStrings from '../SourceControlStrings/SourceControlStrings.ts'
 
 export const getMenuEntries = (): readonly MenuEntry[] => {
-  return [
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.openChanges(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.openFile(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.openFileHead(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.discardChanges(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.stageChanges(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.addToGitignore(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.revealInExplorerView(),
-    },
-    {
-      command: /* TODO */ '-1',
-      flags: MenuItemFlags.None,
-      id: '',
-      label: ViewletSourceControlStrings.openContainingFolder(),
-    },
-  ]
+  return []
 }

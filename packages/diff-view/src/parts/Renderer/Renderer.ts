ba69db8a297d10a3b103006e102fb54ffd5280dd
Le Vivilet <levivilet@pm.me>
2026-04-16 17:54:51 +0200

    fix

--- packages/diff-view/src/parts/Renderer/Renderer.ts
+++ packages/diff-view/src/parts/Renderer/Renderer.ts
@@ -1,4 +1,4 @@
-import { DiffViewState } from '../SourceControlState/SourceControlState.ts'
+import type { DiffViewState } from '../SourceControlState/SourceControlState.ts'
 
 export interface Renderer {
   (oldState: DiffViewState, newState: DiffViewState): readonly any[]

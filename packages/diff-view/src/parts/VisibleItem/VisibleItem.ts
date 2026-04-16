83c3c1bf6309b581c34069dffac27a11d6ee475f
Le Vivilet <levivilet@pm.me>
2026-04-16 17:28:59 +0200

    fix

--- packages/diff-view/src/parts/VisibleItem/VisibleItem.ts
+++
@@ -1,8 +0,0 @@
-import type { ActionButton } from '../ActionButton/ActionButton.ts'
-import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
-
-export interface VisibleItem extends DisplayItem {
-  readonly buttons: readonly ActionButton[]
-  readonly fileIcon: string
-  readonly indent: number
-}

ebd0683ed27485596a726523b44ad1b490211d12
Le Vivilet <levivilet@pm.me>
2026-04-16 17:57:48 +0200

    fix

--- packages/diff-view/src/parts/RenderItems/RenderItems.ts
+++ packages/diff-view/src/parts/RenderItems/RenderItems.ts
@@ -3,10 +3,10 @@ import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
 import * as GetSourceControlDom from '../GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts'
 
 export const renderItems = (oldState: DiffViewState, newState: DiffViewState): any => {
-  const { id, initial, inputPlaceholder, splitButtonEnabled, visibleItems } = newState
+  const { id, initial } = newState
   if (initial) {
     return [ViewletCommand.SetDom2, id, []]
   }
-  const dom = GetSourceControlDom.getSourceControlVirtualDom(visibleItems, splitButtonEnabled, inputPlaceholder)
+  const dom = GetSourceControlDom.getSourceControlVirtualDom()
   return [ViewletCommand.SetDom2, id, dom]
 }

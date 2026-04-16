67e0a753fdc91369630f33791ad43d15336f2370
Le Vivilet <levivilet@pm.me>
2026-04-16 17:59:05 +0200

    fix

--- packages/diff-view/src/parts/DiffViewState/DiffViewState.ts
+++ packages/diff-view/src/parts/DiffViewState/DiffViewState.ts
@@ -1,37 +1,16 @@
 export interface DiffViewState {
   readonly assetDir: string
-  readonly badgeCount: number
-  readonly decorationIcons: readonly string[]
   readonly deltaY: number
-  readonly enabledProviderIds: readonly string[]
-  readonly expandedGroups: Readonly<Record<string, boolean>>
   readonly finalDeltaY: number
   readonly focus: number
   readonly gitRoot: string
   readonly handleOffset: number
   readonly headerHeight: number
   readonly height: number
-  readonly history: readonly string[]
-  readonly iconDefinitions: readonly string[]
   readonly id: number
-  readonly index: readonly any[]
   readonly initial: boolean
-  readonly inputBoxHeight: number
-  readonly inputBoxMaxHeight: number
-  readonly inputFontFamily: string
-  readonly inputFontSize: number
-  readonly inputFontWeight: number
-  readonly inputLetterSpacing: number
-  readonly inputLineHeight: number
-  readonly inputPadding: number
-  readonly inputPlaceholder: string
-  readonly inputSource: number
-  readonly inputValue: string
-  readonly isVisible: boolean
-  readonly itemHeight: number
   readonly maxInputLines: number
   readonly maxLineY: number
-  readonly merge: readonly any[]
   readonly minimumSliderSize: number
   readonly minLineY: number
   readonly platform: number
@@ -40,9 +19,7 @@ export interface DiffViewState {
   readonly scrollBarActive: boolean
   readonly scrollBarHeight: number
   readonly splitButtonEnabled: boolean
-  readonly untracked: readonly any[]
   readonly width: number
-  readonly workingTree: readonly any[]
   readonly workspacePath: string
   readonly x: number
   readonly y: number

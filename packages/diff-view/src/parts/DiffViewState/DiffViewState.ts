466ab39b35ea67ba3104117f92823cfb3b5f4bc3
Le Vivilet <levivilet@pm.me>
2026-04-16 17:59:57 +0200

    fix

--- packages/diff-view/src/parts/DiffViewState/DiffViewState.ts
+++ packages/diff-view/src/parts/DiffViewState/DiffViewState.ts
@@ -1,24 +1,13 @@
 export interface DiffViewState {
   readonly assetDir: string
-  readonly deltaY: number
-  readonly finalDeltaY: number
   readonly focus: number
-  readonly gitRoot: string
-  readonly handleOffset: number
-  readonly headerHeight: number
   readonly height: number
   readonly id: number
   readonly initial: boolean
-  readonly maxInputLines: number
   readonly maxLineY: number
-  readonly minimumSliderSize: number
   readonly minLineY: number
   readonly platform: number
-  readonly providerId: string
   readonly root: string
-  readonly scrollBarActive: boolean
-  readonly scrollBarHeight: number
-  readonly splitButtonEnabled: boolean
   readonly width: number
   readonly workspacePath: string
   readonly x: number

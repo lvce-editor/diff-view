56866bb97ebec4288da325dbde3c75b63cada50c
Le Vivilet <levivilet@pm.me>
2026-04-16 17:45:10 +0200

    fix

--- packages/diff-view/src/parts/OpenUri/OpenUri.ts
+++
@@ -1,5 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const openUri = (uri: string): Promise<void> => {
-  return RendererWorker.openUri(uri)
-}
--- packages/diff-view/src/parts/RequestFileIcons/RequestFileIcons.ts
+++
@@ -1,7 +0,0 @@
-import { RendererWorker as ParentRpc } from '@lvce-editor/rpc-registry'
-import type { IconRequest } from '../IconRequest/IconRequest.ts'
-
-export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
-  const results = await ParentRpc.invoke('IconTheme.getIcons', requests)
-  return results
-}

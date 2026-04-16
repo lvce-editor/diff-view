0cc8d1ccc989d0d840dd2a6e41f208bc9c81db76
Le Vivilet <levivilet@pm.me>
2026-04-16 17:44:09 +0200

    unused

--- packages/diff-view/src/parts/SourceControlStates/SourceControlStates.ts
+++
@@ -1,4 +0,0 @@
-import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
-import type { DiffViewState } from '../SourceControlState/SourceControlState.ts'
-
-export const { get, getCommandIds, registerCommands, set, wrapCommand, wrapGetter } = ViewletRegistry.create<DiffViewState>()

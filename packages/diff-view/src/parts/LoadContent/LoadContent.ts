db1b360bf4a9ee6ce5217f28730e0192bc4dcc24
Le Vivilet <levivilet@pm.me>
2026-04-18 10:47:14 +0200

    feature: update loadContent to set deltaY, initial, and minLineY properties

--- packages/diff-view/src/parts/LoadContent/LoadContent.ts
+++ packages/diff-view/src/parts/LoadContent/LoadContent.ts
@@ -3,5 +3,8 @@ import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
 export const loadContent = async (state: DiffViewState, savedState: unknown): Promise<DiffViewState> => {
   return {
     ...state,
+    deltaY: 10,
+    initial: false,
+    minLineY: 10,
   }
 }

02bef21c66dd4604db220cc336262066e2c53eeb
Le Vivilet <levivilet@pm.me>
2026-04-18 10:56:38 +0200

    feature: add initializeDiffWorker function to set up DiffWorker with RPC

---
+++ packages/diff-view/src/parts/InitializeDiffWorker/InitializeDiffWorker.ts
@@ -0,0 +1,12 @@
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+
+export const initializeDiffWorker = async (): Promise<void> => {
+  const rpc = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    send(port) {
+      return RendererWorker.sendMessagePortToDiffWorker(port, 0)
+    },
+  })
+  DiffWorker.set(rpc)
+}

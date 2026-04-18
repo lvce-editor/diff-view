b4b93d79bfdaad89934924d2b846399778537e0e
Le Vivilet <levivilet@pm.me>
2026-04-18 11:47:02 +0200

    fix

--- packages/diff-view/src/parts/GetContentRightDom/GetContentRightDom.ts
+++ packages/diff-view/src/parts/GetContentRightDom/GetContentRightDom.ts
@@ -1,6 +1,6 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getContentDom } from '../GetContentDom/GetContentDom.ts'
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 
 export const getContentRightDom = (contentRight: string, errorMessage = '', errorStack = ''): readonly VirtualDomNode[] => {
   return getContentDom(ClassNames.DiffEditorContentRight, contentRight, errorMessage, errorStack)

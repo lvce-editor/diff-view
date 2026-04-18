d5e888dd64162a574f0d4806f2303e9872640cb0
Le Vivilet <levivilet@pm.me>
2026-04-18 10:47:45 +0200

    feature: update className in getSourceControlVirtualDom to 'Viewlet DiffEditor2'

--- packages/diff-view/src/parts/GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts
+++ packages/diff-view/src/parts/GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts
@@ -5,7 +5,7 @@ export const getSourceControlVirtualDom = (): readonly VirtualDomNode[] => {
   const dom: readonly VirtualDomNode[] = [
     {
       childCount: 1,
-      className: 'Viewlet Diff',
+      className: 'Viewlet DiffEditor2',
       type: VirtualDomElements.Div,
     },
     text('hello world'),

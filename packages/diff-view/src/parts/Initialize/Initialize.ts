70af2d2141217798456f66e421ff1ec89c816406
Le Vivilet <levivilet@pm.me>
2026-04-18 12:31:18 +0200

    worker

--- packages/diff-view/src/parts/Initialize/Initialize.ts
+++ packages/diff-view/src/parts/Initialize/Initialize.ts
@@ -1,8 +1,15 @@
 import { createExtensionHostRpc } from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
 import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
+import { initializeDiffWorker } from '../InitializeDiffWorker/InitializeDiffWorker.ts'
 import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  await Promise.all([createExtensionHostRpc(), createTextMeasurementWorkerRpc(), initializeExtensionManagementWorker(), initializeFileSystemWorker()])
+  await Promise.all([
+    createExtensionHostRpc(),
+    createTextMeasurementWorkerRpc(),
+    initializeExtensionManagementWorker(),
+    initializeFileSystemWorker(),
+    initializeDiffWorker(),
+  ])
 }

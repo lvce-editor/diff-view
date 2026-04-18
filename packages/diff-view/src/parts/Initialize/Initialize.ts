9e0afde5bb1e7af6e2deabb403388f07762a8339
Le Vivilet <levivilet@pm.me>
2026-04-18 11:03:50 +0200

    fix

--- packages/diff-view/src/parts/Initialize/Initialize.ts
+++ packages/diff-view/src/parts/Initialize/Initialize.ts
@@ -2,9 +2,15 @@ import { ExtensionHost, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 import { createExtensionHostRpc } from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
 import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
 import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
+import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  const [extensionHostRpc, textRpc] = await Promise.all([createExtensionHostRpc(), createTextMeasurementWorkerRpc(), initializeExtensionManagementWorker()])
+  const [extensionHostRpc, textRpc] = await Promise.all([
+    createExtensionHostRpc(),
+    createTextMeasurementWorkerRpc(),
+    initializeExtensionManagementWorker(),
+    initializeFileSystemWorker(),
+  ])
   ExtensionHost.set(extensionHostRpc)
   TextMeasurementWorker.set(textRpc)
 }

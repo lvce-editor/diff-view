92b40cc7d912a30e98df99a028fc8d9100b070c6
Le Vivilet <levivilet@pm.me>
2026-04-18 10:46:21 +0200

    feature: update commandMap to include additional commands (#4)

--- packages/diff-view/src/parts/CommandMap/CommandMap.ts
+++ packages/diff-view/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,19 @@
+import { terminate } from '@lvce-editor/viewlet-registry'
 import * as Create2 from '../Create2/Create2.ts'
+import { diff2 } from '../Diff2/Diff2.ts'
+import { wrapCommand } from '../DiffViewStates/DiffViewStates.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
+import { loadContent } from '../LoadContent/LoadContent.ts'
+import { render2 } from '../Render2/Render2.ts'
+import { saveState } from '../SaveState/SaveState.ts'
 
 export const commandMap = {
+  'DiffView.create': Create2.create2,
+  'DiffView.diff2': diff2,
   'DiffView.initialize': Initialize.initialize,
+  'DiffView.loadContent': wrapCommand(loadContent),
+  'DiffView.render2': render2,
+  'DiffView.saveState': saveState,
+  'DiffView.terminate': terminate,
   'Initialize.initialize': Initialize.initialize,
-  'SourceControl.create2': Create2.create2,
 }

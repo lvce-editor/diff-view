df5454984472630657ae3aa908bccbd028c9d480
Le Vivilet <levivilet@pm.me>
2026-04-18 10:37:46 +0200

    feature: add 'DiffView.initialize' command to commandMap

--- packages/diff-view/src/parts/CommandMap/CommandMap.ts
+++ packages/diff-view/src/parts/CommandMap/CommandMap.ts
@@ -2,6 +2,7 @@ import * as Create2 from '../Create2/Create2.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 
 export const commandMap = {
+  'DiffView.initialize': Initialize.initialize,
   'Initialize.initialize': Initialize.initialize,
   'SourceControl.create2': Create2.create2,
 }

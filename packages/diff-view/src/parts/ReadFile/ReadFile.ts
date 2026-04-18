31a58b32188473624f8e86d1787b989104e51437
Le Vivilet <levivilet@pm.me>
2026-04-18 11:04:54 +0200

    fix

--- packages/diff-view/src/parts/ReadFile/ReadFile.ts
+++ packages/diff-view/src/parts/ReadFile/ReadFile.ts
@@ -1,4 +1,4 @@
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
+import { ExtensionHost, FileSystemWorker } from '@lvce-editor/rpc-registry'
 import { getPath } from '../GetPath/GetPath.ts'
 import { getProtocol } from '../GetProtocol/GetProtocol.ts'
 
@@ -9,6 +9,9 @@ export const readFile = async (uri: string): Promise<string> => {
   const protocol = getProtocol(uri)
   if (protocol === 'data') {
     return uri.slice('data://'.length)
+  }
+  if (protocol === 'file') {
+    return FileSystemWorker.readFile(uri)
   }
   const path = getPath(protocol, uri)
   return ExtensionHost.invoke('ExtensionHostFileSystem.readFile', protocol, path)

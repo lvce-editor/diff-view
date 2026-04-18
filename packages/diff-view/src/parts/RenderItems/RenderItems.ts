791b01d1c76732e599002f09f941fe22052c708d
Le Vivilet <levivilet@pm.me>
2026-04-18 10:54:04 +0200

    feature: implement getDiffEditorVirtualDom and update renderItems to use it (#8)

--- packages/diff-view/src/parts/RenderItems/RenderItems.ts
+++ packages/diff-view/src/parts/RenderItems/RenderItems.ts
@@ -1,12 +1,12 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
-import * as GetSourceControlDom from '../GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts'
+import * as GetDiffEditorDom from '../GetDiffEditorVirtualDom/GetDiffEditorVirtualDom.ts'
 
 export const renderItems = (oldState: DiffViewState, newState: DiffViewState): any => {
   const { id, initial } = newState
   if (initial) {
     return [ViewletCommand.SetDom2, id, []]
   }
-  const dom = GetSourceControlDom.getSourceControlVirtualDom()
+  const dom = GetDiffEditorDom.getDiffEditorVirtualDom()
   return [ViewletCommand.SetDom2, id, dom]
 }

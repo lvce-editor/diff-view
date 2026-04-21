c63e592a4b3fefa23403d71c93acd7776bf5267c
Le Vivilet <levivilet@pm.me>
2026-04-21 16:37:03 +0200

    fix: update locators in diff-editor-insertion test for accurate element targeting

--- packages/e2e/src/viewlet.diff-editor-insertion.ts
+++ packages/e2e/src/viewlet.diff-editor-insertion.ts
@@ -15,12 +15,12 @@ export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace 
   await Main.openUri(`diff://${tmpDir}/file-1.txt<->${tmpDir}/file-2.txt`)
 
   // assert
-  const contentLeft = Locator('.DiffEditorContentLeft')
-  const contentRight = Locator('.DiffEditorContentRight')
+  const contentLeft = Locator('.DiffEditorContentLeft .DiffEditorRows')
+  const contentRight = Locator('.DiffEditorContentRight .DiffEditorRows')
   await expect(contentLeft).toHaveText('')
   await expect(contentRight).toHaveText('def')
-  const rowLeft = contentLeft.locator('.EditorRow')
+  const rowLeft = contentLeft.locator('.EditorRow').nth(0)
   await expect(rowLeft).toHaveClass('Deletion')
-  const rowRight = contentRight.locator('.EditorRow')
+  const rowRight = contentRight.locator('.EditorRow').nth(1) // TODO
   await expect(rowRight).toHaveClass('Insertion')
 }

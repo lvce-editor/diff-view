b55964d72fc4d0cdf80eedb75b6c299e3781905f
Le Vivilet <levivilet@pm.me>
2026-04-21 16:17:16 +0200

    feature: update ESLint configuration to disable spellchecker for generated TypeScript files (#73)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,13 @@
 import config from '@lvce-editor/eslint-config'
 import actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config, ...actions]
+export default [
+  ...config,
+  ...actions,
+  {
+    files: ['packages/e2e/src/generated/**/*.ts'],
+    rules: {
+      '@cspell/spellchecker': 'off',
+    },
+  },
+]

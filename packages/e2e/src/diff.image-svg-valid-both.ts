11021c4d86815c4e6427fc6935ac64709674d817
Le Vivilet <levivilet@pm.me>
2026-04-21 21:42:52 +0200

    fix

--- packages/e2e/src/diff.image-svg-valid-both.ts
+++ packages/e2e/src/diff.image-svg-valid-both.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'diff.image-svg-valid-both'
 
+export const skip = 1
+
 export const test: Test = async (api) => {
   const { FileSystem, Locator, Main } = api
   const tmpDir = await FileSystem.getTmpDir()

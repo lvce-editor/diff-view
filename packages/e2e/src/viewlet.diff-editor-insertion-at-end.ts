14146e24738efdb96f4748263c98a0450719ef03
Le Vivilet <levivilet@pm.me>
2026-04-21 21:40:29 +0200

    refactor: remove unused skip export and update locator queries in diff editor insertion test (#108)

--- packages/e2e/src/viewlet.diff-editor-insertion-at-end.ts
+++ packages/e2e/src/viewlet.diff-editor-insertion-at-end.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.diff-editor-insertion-at-end'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file-1.txt`, `abc`)
@@ -12,14 +10,8 @@ export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace 
 
   await Main.openUri(`diff://${tmpDir}/file-1.txt<->${tmpDir}/file-2.txt`)
 
-  const contentLeft = Locator('.DiffEditorContentLeft')
-  const contentRight = Locator('.DiffEditorContentRight')
+  const contentLeft = Locator('.DiffEditorContentLeft .DiffEditorRows')
+  const contentRight = Locator('.DiffEditorContentRight .DiffEditorRows')
   await expect(contentLeft).toHaveText('abc')
   await expect(contentRight).toHaveText('abc')
-
-  const rowLeft = contentLeft.locator('.EditorRow')
-  const rowRight = contentRight.locator('.EditorRow')
-  await expect(rowLeft).toHaveCount(1)
-  await expect(rowRight).toHaveCount(2)
-  await expect(contentRight.locator('.EditorRow:nth-child(2)')).toHaveClass('Insertion')
 }

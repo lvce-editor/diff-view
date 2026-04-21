8748a9e0cbc29651b46c583c302c77c21c079d67
Le Vivilet <levivilet@pm.me>
2026-04-21 21:27:10 +0200

    refactor: remove unused skip export from diff editor deletion test (#100)

--- packages/e2e/src/viewlet.diff-editor-deletion.ts
+++ packages/e2e/src/viewlet.diff-editor-deletion.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.diff-editor-deletion'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

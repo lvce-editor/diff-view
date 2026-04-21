c5aa81691f82c8b8d6a4921c6633e8feff5cf0e4
Le Vivilet <levivilet@pm.me>
2026-04-21 23:48:57 +0200

    feat: update large text file test to use before and after text files

--- packages/e2e/src/diff.large-txt-file.ts
+++ packages/e2e/src/diff.large-txt-file.ts
@@ -2,29 +2,17 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'diff.large-text-file'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const fixtureUrl = import.meta.resolve('../fixtures/large-text-file')
   const tmpDir = await FileSystem.loadFixture(fixtureUrl)
   await Workspace.setPath(tmpDir)
 
-  await Main.openUri(`diff://${tmpDir}/left.json<->${tmpDir}/right.json`)
+  await Main.openUri(`diff://${tmpDir}/before.txt<->${tmpDir}/after.txt`)
 
-  const beforePane = Locator('.DiffEditorContentLeft .DiffEditorRows')
-  const afterPane = Locator('.DiffEditorContentRight .DiffEditorRows')
-  const insertedRows = Locator('.DiffEditorContentRight .DiffRow--inserted')
+  const beforeDeletion = Locator('.EditorRow.Deletion')
+  await expect(beforeDeletion).toHaveText('a')
 
-  await expect(beforePane).toContainText('"id": 1,')
-  await expect(afterPane).toContainText('"id": 2,')
-  await expect(afterPane).toContainText('"name": "bravo"')
-  await expect(insertedRows).toHaveCount(7)
-  await expect(insertedRows.nth(0)).toHaveText('{')
-  await expect(insertedRows.nth(1)).toHaveText('"id": 2,')
-  await expect(insertedRows.nth(2)).toHaveText('"name": "bravo",')
-  await expect(insertedRows.nth(3)).toHaveText('"meta": {')
-  await expect(insertedRows.nth(4)).toHaveText('"enabled": false')
-  await expect(insertedRows.nth(5)).toHaveText('}')
-  await expect(insertedRows.nth(6)).toHaveText('}')
+  const afterInsertion = Locator('.EditorRow.Insertion')
+  await expect(afterInsertion).toHaveText('b')
 }

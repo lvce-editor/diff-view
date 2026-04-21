b48ca281894cc16cf6d8c9d394c333ddc07655ad
Le Vivilet <levivilet@pm.me>
2026-04-21 23:51:56 +0200

    fix

--- packages/e2e/src/viewlet.diff-editor-insertion.ts
+++ packages/e2e/src/viewlet.diff-editor-insertion.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.diff-editor-insertion'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

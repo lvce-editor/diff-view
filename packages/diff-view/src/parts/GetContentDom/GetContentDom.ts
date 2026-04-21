e3aa7baba7a2160324ac04aeb3868b91739ce7b4
Le Vivilet <levivilet@pm.me>
2026-04-21 22:43:08 +0200

    refactor: separate content DOM generation into distinct functions for line numbers and without line numbers (#139)

--- packages/diff-view/src/parts/GetContentDom/GetContentDom.ts
+++ packages/diff-view/src/parts/GetContentDom/GetContentDom.ts
@@ -1,76 +1,11 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { InlineDiffChange } from '../InlineDiffChange/InlineDiffChange.ts'
 import type { TokenizedLine } from '../TokenizedLine/TokenizedLine.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getContentDomWithLineNumbers } from '../GetContentDomWithLineNumbers/GetContentDomWithLineNumbers.ts'
+import { getContentDomWithoutLineNumbers } from '../GetContentDomWithoutLineNumbers/GetContentDomWithoutLineNumbers.ts'
 import { getErrorDom } from '../GetErrorDom/GetErrorDom.ts'
-import { getLineNumbersDom } from '../GetLineNumbersDom/GetLineNumbersDom.ts'
 import { getRowsDom } from '../GetRowsDom/GetRowsDom.ts'
 import { getVisibleInlineDiffRows } from '../GetVisibleInlineDiffRows/GetVisibleInlineDiffRows.ts'
-
-const getContentDomWithLineNumbers = (
-  contentClassName: string,
-  startLineNumber: number,
-  lineCount: number,
-  rowsChildCount: number,
-  rows: readonly VirtualDomNode[],
-): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.DiffEditorContent,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 2,
-      className: contentClassName,
-      type: VirtualDomElements.Div,
-    },
-    ...getLineNumbersDom(startLineNumber, lineCount),
-    {
-      childCount: rowsChildCount,
-      className: ClassNames.DiffEditorRows,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 0,
-      className: ClassNames.DiffEditorSpacerTop,
-      type: VirtualDomElements.Div,
-    },
-    ...rows,
-    {
-      childCount: 0,
-      className: ClassNames.DiffEditorSpacerBottom,
-      type: VirtualDomElements.Div,
-    },
-  ]
-}
-
-const getContentDomWithoutLineNumbers = (contentClassName: string, rowsChildCount: number, rows: readonly VirtualDomNode[]): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.DiffEditorContent,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: rowsChildCount,
-      className: contentClassName,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 0,
-      className: ClassNames.DiffEditorSpacerTop,
-      type: VirtualDomElements.Div,
-    },
-    ...rows,
-    {
-      childCount: 0,
-      className: ClassNames.DiffEditorSpacerBottom,
-      type: VirtualDomElements.Div,
-    },
-  ]
-}
 
 export const getContentDom = (
   contentClassName: string,

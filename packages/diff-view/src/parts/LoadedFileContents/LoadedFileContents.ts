0a2e68a452c64ac5d3e56c2c7e5c8533e01e71c1
Le Vivilet <levivilet@pm.me>
2026-04-21 22:51:23 +0200

    refactor: move LoadedFileContents interface to its own file

---
+++ packages/diff-view/src/parts/LoadedFileContents/LoadedFileContents.ts
@@ -0,0 +1,8 @@
+export interface LoadedFileContents {
+  readonly contentLeft: string
+  readonly contentRight: string
+  readonly errorLeftMessage: string
+  readonly errorLeftStack: string
+  readonly errorRightMessage: string
+  readonly errorRightStack: string
+}

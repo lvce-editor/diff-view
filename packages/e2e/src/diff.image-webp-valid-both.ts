9194de2f893283ac01f0044996a8f481395ec495
Le Vivilet <levivilet@pm.me>
2026-04-21 17:47:48 +0200

    refactor: update test fixtures and URIs for image diff tests

--- packages/e2e/src/diff.image-webp-valid-both.ts
+++ packages/e2e/src/diff.image-webp-valid-both.ts
@@ -13,12 +13,12 @@ export const name = 'diff.image-webp-valid-both'
 
 export const skip = 1
 export const test: Test = async (api) => {
-  const { Command, FileSystem, Locator, Main } = api
+  const { FileSystem, Locator, Main } = api
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/fixture.txt`, 'fixture')
-  await Command.execute('DiffView.setFixture', 'image-webp-valid-both')
+  await FileSystem.writeFile(`${tmpDir}/left.webp`, 'fixture')
+  await FileSystem.writeFile(`${tmpDir}/image-webp-valid-both.webp`, 'fixture')
 
-  await Main.openUri(`${tmpDir}/fixture.txt`)
+  await Main.openUri(`diff://${tmpDir}/left.webp<->${tmpDir}/image-webp-valid-both.webp`)
 
   const beforeImage = Locator('.DiffPane--before .ImageElement')
   const afterImage = Locator('.DiffPane--after .ImageElement')

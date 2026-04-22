26ac0e4142035e4e2f6adfc4e77bb3a63ef862cc
Le Vivilet <levivilet@pm.me>
2026-04-22 08:51:46 +0200

    feat: add test for getScrollBarDom rendering scrollbar track and thumb

---
+++ packages/diff-view/test/GetScrollBarDom.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { getScrollBarDom } from '../src/parts/GetScrollBarDom/GetScrollBarDom.ts'
+
+test('getScrollBarDom renders the scrollbar track and thumb', (): void => {
+  expect(getScrollBarDom()).toEqual([
+    {
+      childCount: 1,
+      className: ClassNames.ScrollBar,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 0,
+      className: ClassNames.ScrollBarThumb,
+      name: 'scrollBarThumb',
+      onPointerDown: DomEventListenerFunctions.HandleScrollBarPointerDown,
+      type: VirtualDomElements.Div,
+    },
+  ])
+})

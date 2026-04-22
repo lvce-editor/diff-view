7963c300b8e428cdc18634423f1b8b3eb818dee2
Le Vivilet <levivilet@pm.me>
2026-04-22 17:35:44 +0200

    refactor: update diff editor insertion test to improve row and line number assertions (#241)

--- packages/e2e/src/viewlet.diff-editor-insertion-at-start-and-end.ts
+++ packages/e2e/src/viewlet.diff-editor-insertion-at-start-and-end.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.diff-editor-insertion-at-start-and-end'
 
+export const skip = 1
+
 export const test: Test = async ({ DiffView, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -19,8 +21,28 @@ d`,
   await DiffView.open(`${tmpDir}/file-1.txt`, `${tmpDir}/file-2.txt`)
 
   // assert
-  const contentLeft = Locator('.DiffEditorContentLeft .DiffEditorRows')
-  const contentRight = Locator('.DiffEditorContentRight .DiffEditorRows')
-  await expect(contentLeft).toHaveText('c')
-  await expect(contentRight).toHaveText('aabcd') // TODO
+  const leftRows = Locator('.DiffEditorContentLeft .EditorRow')
+  const rightRows = Locator('.DiffEditorContentRight .EditorRow')
+  const leftLineNumbers = Locator('.DiffEditorContentLeft .DiffEditorLineNumber')
+  const rightLineNumbers = Locator('.DiffEditorContentRight .DiffEditorLineNumber')
+
+  await expect(leftRows.nth(0)).toHaveText('')
+  await expect(leftRows.nth(1)).toHaveText('')
+  await expect(leftRows.nth(2)).toHaveText('1c')
+  await expect(leftRows.nth(3)).toHaveText('')
+
+  await expect(rightRows.nth(0)).toHaveText('1a')
+  await expect(rightRows.nth(1)).toHaveText('2b')
+  await expect(rightRows.nth(2)).toHaveText('3c')
+  await expect(rightRows.nth(3)).toHaveText('4d')
+
+  await expect(leftLineNumbers.nth(0)).toHaveText('')
+  await expect(leftLineNumbers.nth(1)).toHaveText('')
+  await expect(leftLineNumbers.nth(2)).toHaveText('1')
+  await expect(leftLineNumbers.nth(3)).toHaveText('')
+
+  await expect(rightLineNumbers.nth(0)).toHaveText('1')
+  await expect(rightLineNumbers.nth(1)).toHaveText('2')
+  await expect(rightLineNumbers.nth(2)).toHaveText('3')
+  await expect(rightLineNumbers.nth(3)).toHaveText('4')
 }

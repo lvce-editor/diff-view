63e13c3f7b3dfdbd805e19017a19ca630f2aeac6
Le Vivilet <levivilet@pm.me>
2026-04-22 08:52:50 +0200

    feat: add tests for diff function to validate state changes and equality

---
+++ packages/diff-view/test/Diff.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { diff } from '../src/parts/Diff/Diff.ts'
+import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+
+test('diff returns the renderers for changed state groups', (): void => {
+  const oldState = createDefaultState()
+  const newState = {
+    ...oldState,
+    deltaY: 1,
+    focus: 1,
+    height: 200,
+    inputValue: 'changed',
+  }
+
+  expect(diff(oldState, newState)).toEqual([DiffType.RenderIncremental, DiffType.RenderValue, DiffType.RenderCss, DiffType.RenderFocusContext])
+})
+
+test('diff returns no renderers when states are equal', (): void => {
+  const state = createDefaultState()
+
+  expect(diff(state, state)).toEqual([])
+})

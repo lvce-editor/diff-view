ab5c02111f357bea7ca5697d9962aa35665b43da
Le Vivilet <levivilet@pm.me>
2026-04-22 09:25:07 +0200

    feat: implement GetVisibleLinesDom and associated helper functions with tests (#180)

--- packages/diff-view/src/parts/GetLineNumbersDom/GetLineNumbersDom.ts
+++ packages/diff-view/src/parts/GetLineNumbersDom/GetLineNumbersDom.ts
@@ -4,12 +4,14 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getLineNumberDom } from '../GetLineNumberDom/GetLineNumberDom.ts'
 
 export const getLineNumbersDom = (startLineNumber: number, lineCount: number): readonly VirtualDomNode[] => {
+  const lineNumbers = Array.from({ length: lineCount }, (_, index) => startLineNumber + index)
+
   return [
     {
       childCount: lineCount,
       className: ClassNames.DiffEditorGutter,
       type: VirtualDomElements.Div,
     },
-    ...Array.from({ length: lineCount }, (_, index) => getLineNumberDom(startLineNumber + index)).flat(),
+    ...lineNumbers.flatMap((lineNumber) => getLineNumberDom(lineNumber)),
   ]
 }
--- packages/diff-view/src/parts/GetVisibleLinesDom/GetVisibleLinesDom.ts
+++ packages/diff-view/src/parts/GetVisibleLinesDom/GetVisibleLinesDom.ts
@@ -1,46 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { VisibleLine, VisibleLineToken } from '../VisibleLine/VisibleLine.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { VisibleLineType } from '../VisibleLine/VisibleLine.ts'
+import type { VisibleLine } from '../VisibleLine/VisibleLine.ts'
+import { getLineDom } from './GetLineDom/GetLineDom.ts'
 
 type DiffSide = 'left' | 'right'
-
-const getTokenDom = (token: VisibleLineToken): readonly VirtualDomNode[] => {
-  if (!token.type) {
-    return [text(token.text)]
-  }
-  return [
-    {
-      childCount: 1,
-      className: token.type,
-      type: VirtualDomElements.Span,
-    },
-    text(token.text),
-  ]
-}
-
-const getRowClassName = (type: VisibleLine['type'], side: DiffSide): string => {
-  if (type === VisibleLineType.Removed) {
-    return ClassNames.EditorRowDeletion
-  }
-  if (type === VisibleLineType.Added) {
-    return ClassNames.EditorRowInsertion
-  }
-  return ClassNames.EditorRow
-}
-
-const getLineDom = (line: VisibleLine, side: DiffSide): readonly VirtualDomNode[] => {
-  const children = line.tokens.length === 0 ? [text('')] : line.tokens.flatMap(getTokenDom)
-  return [
-    {
-      childCount: children.length,
-      className: getRowClassName(line.type, side),
-      type: VirtualDomElements.Div,
-    },
-    ...children,
-  ]
-}
 
 export const getVisibleLinesDom = (visibleLines: readonly VisibleLine[], side: DiffSide): readonly VirtualDomNode[] => {
   return visibleLines.flatMap((line) => getLineDom(line, side))

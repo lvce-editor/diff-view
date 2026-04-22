c8ceac7a4fddbcbe1a2466ecafefd99b5b00e7e5
Le Vivilet <levivilet@pm.me>
2026-04-22 13:58:13 +0200

    refactor: simplify getVisibleLinesDom and getLineDom by removing unused parameters

--- packages/diff-view/src/parts/GetVisibleLinesDom/GetVisibleLinesDom.ts
+++ packages/diff-view/src/parts/GetVisibleLinesDom/GetVisibleLinesDom.ts
@@ -2,8 +2,6 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleLine } from '../VisibleLine/VisibleLine.ts'
 import { getLineDom } from './GetLineDom/GetLineDom.ts'
 
-type DiffSide = 'left' | 'right'
-
-export const getVisibleLinesDom = (visibleLines: readonly VisibleLine[], side: DiffSide): readonly VirtualDomNode[] => {
-  return visibleLines.flatMap((line) => getLineDom(line, side))
+export const getVisibleLinesDom = (visibleLines: readonly VisibleLine[]): readonly VirtualDomNode[] => {
+  return visibleLines.flatMap(getLineDom)
 }

80885465474d5d837005afc48d943e1bb5e55d9d
Le Vivilet <levivilet@pm.me>
2026-04-22 21:26:16 +0200

    refactor: simplify RPC creation by removing error handling in extension management worker initialization

--- packages/diff-view/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
+++ packages/diff-view/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
@@ -2,10 +2,6 @@ import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import { createExtensionManagementWorkerRpc } from '../CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts'
 
 export const initializeExtensionManagementWorker = async (): Promise<void> => {
-  try {
-    const rpc = await createExtensionManagementWorkerRpc()
-    ExtensionManagementWorker.set(rpc)
-  } catch {
-    // ignore
-  }
+  const rpc = await createExtensionManagementWorkerRpc()
+  ExtensionManagementWorker.set(rpc)
 }

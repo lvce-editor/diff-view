6bde0b200013bae383c343d8d86a62712c6c7d15
Le Vivilet <levivilet@pm.me>
2026-04-22 17:36:53 +0200

    feat: add test for diffing a file with 250,000 lines added (#242)

---
+++ packages/e2e/src/diff.two-hundred-fifty-thousand-lines-added.ts
@@ -0,0 +1,29 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+const getContent = (): string => {
+  const lines: string[] = []
+
+  for (let lineNumber = 1; lineNumber <= 250_000; lineNumber++) {
+    lines.push(`line ${lineNumber}`)
+  }
+
+  return lines.join('\n')
+}
+
+export const name = 'diff.two-hundred-fifty-thousand-lines-added'
+
+export const test: Test = async ({ DiffView, expect, FileSystem, Locator, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file-1.txt`, ``)
+  await FileSystem.writeFile(`${tmpDir}/file-2.txt`, getContent())
+  await Workspace.setPath(tmpDir)
+
+  await DiffView.open(`${tmpDir}/file-1.txt`, `${tmpDir}/file-2.txt`)
+
+  const contentLeft = Locator('.DiffEditorContentLeft .DiffEditorRows')
+  const contentRight = Locator('.DiffEditorContentRight .DiffEditorRows')
+
+  await expect(contentLeft).toHaveText('')
+  await expect(contentRight).toContainText('line 1')
+  await expect(Locator('.ScrollBar')).toHaveCount(1)
+}

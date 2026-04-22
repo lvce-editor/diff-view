6f60e6bed384bb898bfc46753c20fbcb16d88583
Le Vivilet <levivilet@pm.me>
2026-04-22 09:13:25 +0200

    refactor: simplify getLineNumberDom by extracting parentNode definition (#172)

--- packages/diff-view/src/parts/GetLineNumberDom/GetLineNumberDom.ts
+++ packages/diff-view/src/parts/GetLineNumberDom/GetLineNumberDom.ts
@@ -2,13 +2,12 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 
+const parentNode = {
+  childCount: 1,
+  className: ClassNames.DiffEditorLineNumber,
+  type: VirtualDomElements.Div,
+}
+
 export const getLineNumberDom = (lineNumber: number): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.DiffEditorLineNumber,
-      type: VirtualDomElements.Div,
-    },
-    text(String(lineNumber)),
-  ]
+  return [parentNode, text(String(lineNumber))]
 }

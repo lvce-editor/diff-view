858c1e68639dae664be1ecd51314032dd302ce9d
Le Vivilet <levivilet@pm.me>
2026-04-22 14:04:53 +0200

    fix

--- packages/diff-view/src/parts/GetInlineDiffEditorVirtualDom/GetInlineDiffLineNumberDom/GetInlineDiffLineNumberDom.ts
+++ packages/diff-view/src/parts/GetInlineDiffEditorVirtualDom/GetInlineDiffLineNumberDom/GetInlineDiffLineNumberDom.ts
@@ -1,7 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import * as ClassNames from '../../ClassNames/ClassNames.ts'
 import type { InlineDiffRow } from '../../GetInlineDiffRows/GetInlineDiffRows.ts'
+import * as ClassNames from '../../ClassNames/ClassNames.ts'
 import { getInlineDiffLineNumberText } from '../GetInlineDiffLineNumberText/GetInlineDiffLineNumberText.ts'
 
 export const getInlineDiffLineNumberDom = (row: InlineDiffRow): readonly VirtualDomNode[] => {

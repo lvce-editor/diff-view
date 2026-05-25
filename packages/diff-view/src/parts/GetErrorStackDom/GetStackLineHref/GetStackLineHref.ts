df5c0c52b5aad2de3460b2e0d81935a814084544
Le Vivilet <levivilet@pm.me>
2026-05-25 16:10:19 +0200

    fix

--- packages/diff-view/src/parts/GetErrorStackDom/GetStackLineHref/GetStackLineHref.ts
+++ packages/diff-view/src/parts/GetErrorStackDom/GetStackLineHref/GetStackLineHref.ts
@@ -1,7 +1,11 @@
 export const getStackLineHref = (location: string, allowedLinkSchemes: readonly string[]): string => {
   const scheme = location.startsWith('/') ? 'file' : location.slice(0, location.indexOf(':'))
   if (allowedLinkSchemes.includes(scheme)) {
-    return scheme === 'file' ? `file://${location.startsWith('file://') ? location.slice('file://'.length) : location}` : location
+    if (scheme !== 'file') {
+      return location
+    }
+    const fileLocation = location.startsWith('file://') ? location.slice('file://'.length) : location
+    return `file://${fileLocation}`
   }
   return '#'
 }

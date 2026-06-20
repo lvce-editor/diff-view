2bf6d4f91fbaff3753c0e48e9a1cbb1492601e29
Le Vivilet <levivilet@pm.me>
2026-06-20 17:11:21 +0200

    fix

--- packages/e2e/src/diff.context-menu-paste.ts
+++ packages/e2e/src/diff.context-menu-paste.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'diff.context-menu-paste'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, ContextMenu, DiffView, expect, FileSystem, Locator, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file-1.txt`, `const value = before`)

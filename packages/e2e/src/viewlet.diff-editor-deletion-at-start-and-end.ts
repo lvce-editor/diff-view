71a4692b1c573c79bc8f240103e51a7d67d16982
Le Vivilet <levivilet@pm.me>
2026-06-20 23:16:00 +0200

    e2e

--- packages/e2e/src/viewlet.diff-editor-deletion-at-start-and-end.ts
+++ packages/e2e/src/viewlet.diff-editor-deletion-at-start-and-end.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.diff-editor-deletion-at-start-and-end'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ DiffView, expect, FileSystem, Locator, Workspace }) => {
   // arrange

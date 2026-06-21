24e70dd070821bf13359df897ad84d0bc04e7f9f
Le Vivilet <levivilet@pm.me>
2026-06-21 18:34:11 +0200

    Add per-side editor cursor state (#341) (#343)

--- packages/e2e/src/diff.css-three-lines-added-light-theme.ts
+++ packages/e2e/src/diff.css-three-lines-added-light-theme.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'diff.css-three-lines-added-light-theme'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, DiffView, expect, FileSystem, Locator, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(

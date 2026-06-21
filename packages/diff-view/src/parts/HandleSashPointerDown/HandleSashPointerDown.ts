0d609e80242b280b1a136ac1f83eb9d1ceaa30b2
Le Vivilet <levivilet@pm.me>
2026-06-21 20:14:44 +0200

    fix

--- packages/diff-view/src/parts/HandleSashPointerDown/HandleSashPointerDown.ts
+++ packages/diff-view/src/parts/HandleSashPointerDown/HandleSashPointerDown.ts
@@ -1,7 +1,7 @@
 import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
 
 export const handleSashPointerDown = (state: DiffViewState, clientX: number, clientY: number): DiffViewState => {
-  const { layout, leftWidth, x, y } = state
+  const { x, y, layout, leftWidth } = state
   const isVertical = layout === 'vertical'
   return {
     ...state,

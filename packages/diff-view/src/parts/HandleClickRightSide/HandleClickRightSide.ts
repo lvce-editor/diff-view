4b536fb57aecc70d805b04a9ef238d841caee2cb
Le Vivilet <levivilet@pm.me>
2026-06-21 19:43:57 +0200

    fix

--- packages/diff-view/src/parts/HandleClickRightSide/HandleClickRightSide.ts
+++ packages/diff-view/src/parts/HandleClickRightSide/HandleClickRightSide.ts
@@ -1,6 +1,6 @@
 import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
-import { getCursorPositionFromCoordinates } from '../GetCursorPositionFromCoordinates/GetCursorPositionFromCoordinates.ts'
 import * as DiffEditorWhenExpression from '../DiffEditorWhenExpression/DiffEditorWhenExpression.ts'
+import { getCursorPositionFromCoordinates } from '../GetCursorPositionFromCoordinates/GetCursorPositionFromCoordinates.ts'
 
 export const handleClickRightSide = (state: DiffViewState, clientX: number, clientY: number): DiffViewState => {
   const cursorPosition = getCursorPositionFromCoordinates(state, clientX, clientY)

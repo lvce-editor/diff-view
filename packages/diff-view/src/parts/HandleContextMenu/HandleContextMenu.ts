ceb4ae6df1cbd148cb84614502045b7d933665ce
Le Vivilet <levivilet@pm.me>
2026-06-21 20:14:18 +0200

    feature: update menu ids (#351)

--- packages/diff-view/src/parts/HandleContextMenu/HandleContextMenu.ts
+++ packages/diff-view/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -1,10 +1,10 @@
-import { MenuEntryId } from '@lvce-editor/constants'
 import type { DiffViewState } from '../DiffViewState/DiffViewState.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
+import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleContextMenu = async (state: DiffViewState, button: number, clientX: number, clientY: number): Promise<DiffViewState> => {
-  await ContextMenu.show2(state.id, MenuEntryId.SourceControl, clientX, clientY, {
-    menuId: MenuEntryId.SourceControl,
+  await ContextMenu.show2(state.id, MenuEntryId.Diff, clientX, clientY, {
+    menuId: MenuEntryId.Diff,
   })
   return state
 }

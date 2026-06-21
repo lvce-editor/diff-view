daff78116b5c4d81d197c40c796fd135bbcc18ef
Le Vivilet <levivilet@pm.me>
2026-06-21 20:13:27 +0200

    feature: use ui strings (#350)

--- packages/diff-view/src/parts/DiffStrings/DiffStrings.ts
+++ packages/diff-view/src/parts/DiffStrings/DiffStrings.ts
@@ -5,10 +5,42 @@ export const copy = (): string => {
   return I18nString.i18nString(UiStrings.Copy)
 }
 
+export const acceptCurrentChangeAcceptIncomingChangeAcceptBoth = (): string => {
+  return I18nString.i18nString(UiStrings.AcceptCurrentChangeAcceptIncomingChangeAcceptBoth)
+}
+
 export const cut = (): string => {
   return I18nString.i18nString(UiStrings.Cut)
 }
 
+export const hideWhitespace = (): string => {
+  return I18nString.i18nString(UiStrings.HideWhitespace)
+}
+
+export const incomingChange = (): string => {
+  return I18nString.i18nString(UiStrings.IncomingChange)
+}
+
+export const inline = (): string => {
+  return I18nString.i18nString(UiStrings.Inline)
+}
+
 export const paste = (): string => {
   return I18nString.i18nString(UiStrings.Paste)
+}
+
+export const search = (): string => {
+  return I18nString.i18nString(UiStrings.Search)
+}
+
+export const showWhitespace = (): string => {
+  return I18nString.i18nString(UiStrings.ShowWhitespace)
+}
+
+export const sideBySide = (): string => {
+  return I18nString.i18nString(UiStrings.SideBySide)
+}
+
+export const switchToDiff = (label: string): string => {
+  return I18nString.i18nString(UiStrings.SwitchToDiff, { PH1: label })
 }
--- packages/diff-view/src/parts/GetWhitespaceToggleDom/GetWhitespaceToggleDom.ts
+++ packages/diff-view/src/parts/GetWhitespaceToggleDom/GetWhitespaceToggleDom.ts
@@ -2,10 +2,12 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ActionName from '../ActionName/ActionName.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DiffStrings from '../DiffStrings/DiffStrings.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
+/* eslint-disable  sonarjs/no-selector-parameter */
 const getLabel = (showWhitespace: boolean): string => {
-  return showWhitespace ? 'Hide whitespace' : 'Show whitespace'
+  return showWhitespace ? DiffStrings.hideWhitespace() : DiffStrings.showWhitespace()
 }
 
 export const getWhitespaceToggleDom = (showWhitespace: boolean): readonly VirtualDomNode[] => {

e763c66bd4b49ad053f124bb67d1a2146087c831
Le Vivilet <levivilet@pm.me>
2026-06-22 01:10:57 +0200

    fix

--- packages/diff-view/src/parts/GetImageRightDom/GetImageRightDom.ts
+++ packages/diff-view/src/parts/GetImageRightDom/GetImageRightDom.ts
@@ -1,7 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import type { DiffMode } from '../DiffViewState/DiffViewState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getDiffEditorButtonsDom } from '../GetDiffEditorButtonsDom/GetDiffEditorButtonsDom.ts'
 

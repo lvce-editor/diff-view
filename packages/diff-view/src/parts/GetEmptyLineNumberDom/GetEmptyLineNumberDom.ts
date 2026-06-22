b635af0b593171cb6eb0479a64f840466d07f5e7
Le Vivilet <levivilet@pm.me>
2026-06-22 12:34:43 +0200

    feature: enhance empty line number handling with utility classes and CSS generation

--- packages/diff-view/src/parts/GetEmptyLineNumberDom/GetEmptyLineNumberDom.ts
+++ packages/diff-view/src/parts/GetEmptyLineNumberDom/GetEmptyLineNumberDom.ts
@@ -3,10 +3,11 @@ import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getEmptyLineNumberDom = (emptyLineCount: number, itemHeight: number): VirtualDomNode => {
+  const height = emptyLineCount * itemHeight
+
   return {
     childCount: 0,
-    className: ClassNames.DiffEditorLineNumberEmpty,
-    style: `height: ${emptyLineCount * itemHeight}px;`,
+    className: `${ClassNames.DiffEditorLineNumberEmpty} Height-${height}`,
     type: VirtualDomElements.Div,
   }
 }

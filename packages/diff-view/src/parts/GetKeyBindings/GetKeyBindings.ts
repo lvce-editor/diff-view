439df86b9fe566e5f2a1e6f3278a5001ee3b4a77
Le Vivilet <levivilet@pm.me>
2026-06-22 14:19:57 +0200

    feature: remove unused key binding for source control input

--- packages/diff-view/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/diff-view/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,15 +1,9 @@
-import { WhenExpression } from '@lvce-editor/constants'
-import { KeyCode, KeyModifier } from '@lvce-editor/virtual-dom-worker'
-import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
+import { KeyCode } from '@lvce-editor/virtual-dom-worker'
 import * as DiffEditorWhenExpression from '../DiffEditorWhenExpression/DiffEditorWhenExpression.ts'
+import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
 
 export const getKeyBindings = (): readonly KeyBinding[] => {
   return [
-    {
-      command: 'Source Control.acceptInput',
-      key: KeyModifier.CtrlCmd | KeyCode.Enter,
-      when: WhenExpression.FocusSourceControlInput,
-    },
     {
       command: 'DiffView.insertLineBreak',
       key: KeyCode.Enter,

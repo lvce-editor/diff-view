9681c3d1a39410cc7a58d48ee2de7d9a6c469b55
Le Vivilet <levivilet@pm.me>
2026-07-03 21:00:23 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,6 @@ export default [
     files: ['packages/e2e/**/*.ts'],
     rules: {
       '@cspell/spellchecker': 'off',
-      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
     },
   },
 ]

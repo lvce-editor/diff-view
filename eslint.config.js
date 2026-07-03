98af66d12d585c6ec5e6ac2c5fc4e8d6287dc365
Le Vivilet <levivilet@pm.me>
2026-07-03 20:59:58 +0200

    config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
-import * as regex from '@lvce-editor/eslint-plugin-regex'
 
 export default [
   ...config.default,
-  ...actions.default,
-  ...regex.default,
+  ...config.recommendedRegex,
+  ...config.recommendedActions,
   {
     files: ['packages/e2e/**/*.ts'],
     rules: {

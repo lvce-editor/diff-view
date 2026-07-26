3feaf32947bb2955ff6476049278e6bf913bd517
Le Vivilet <levivilet@pm.me>
2026-07-26 09:45:51 +0200

    feature: migrate to npm workspaces (#408)

--- packages/build/src/config.ts
+++ packages/build/src/config.ts
@@ -10,4 +10,4 @@ export const instantiationsPath = join(root, 'packages', 'diff-view')
 
 export const workerPath = join(root, '.tmp', 'dist', 'dist', 'diffViewWorkerMain.js')
 
-export const playwrightPath = pathToFileURL(join(root, 'packages', 'e2e', 'node_modules', 'playwright', 'index.mjs')).toString()
+export const playwrightPath = pathToFileURL(join(root, 'node_modules', 'playwright', 'index.mjs')).toString()
